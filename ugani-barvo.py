4039e9fcb6a7053232b771b605199ab61c7ae772
NinaKl <nina.klobas@gmail.com>
2015-06-10 15:51:33 +0200

    Majhne spremembe

--- ugani-barvo.py
+++ ugani-barvo.py
@@ -1,53 +1,50 @@
+
 from tkinter import *
 import random
+import os
 
 class Besede():
     def __init__(self, master):
 
 #velikost 'platna'
+        
         self.canvas = Canvas(master, width=300, height=300)
         self.canvas.grid(row=2, column=0)
 
 ##navodila + številka levela 
-
 ## gumb escape prekine in ugasne igro
 
 
-
-
-
-
-
-## ********* IGRA ***********
-
-
+        with open(Level_1.txt, 'wt') as f:
+            for x in f:
+                beseda = StringVar(x)
 		
 #self.beseda = beseda, ki jo bomo prikazali - iz datoteke dobimo seznam
 #besed, ki jih bomo prikazovali.
         colors = ["red", "orange", "yellow", "green", "blue", "violet"]
-        Label(master, text="self.beseda", font=("Helvetica", 32), color=random.choice(colors)).grid(row=2)
-
-# ko igralec vpiše besedilo, se naša beseda zamenja
-
-
+        Label(master, text=self.beseda, font=("Helvetica", 32), fg=random.choice(colors)).grid(row=2)
         
 # Polje v katerega pišemo + spremenljivka, ki hrani njegovo vrednost.
         self.vnesi = StringVar()
         polje_vnesi = Entry(master, textvariable=self.vnesi)
         polje_vnesi.grid(row=3)
 
+        
+## ********* IGRA ***********
+        
+    
+#seznam besed ki ga imamo
+    def igra():
+        
+                
 
 
+# igralec vpiše rešitev in preverimo če je pravilna
+            if str(vartext.fg) == vnesi:
+                rezultat += 1
+            else: rezultat = rezultat
 
-
-
-
-# preverimo če je rešitev pravilna
-
-
-
-
-
+# ko igralec vpiše besedilo, se naša beseda zamenja
 
 
 
